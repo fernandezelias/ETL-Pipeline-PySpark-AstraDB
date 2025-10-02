47112b0d5096619008636616bcc8bad190d95c3a
Elías Fernández <117838945+fernandezelias@users.noreply.github.com>
2025-10-02 19:21:09 -0300

    Update etl_utils.py

--- etl_utils.py
+++ etl_utils.py
@@ -1,22 +1,23 @@
 # etl_utils.py
 
-# --- Importación de librerías estándar ---
+# --- Librerías estándar ---
 import os
 import json
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import hashlib
+
+# --- Librerías de terceros ---
+from dotenv import load_dotenv
+from astrapy import DataAPIClient # Librería oficial de Astra DB para Python
 
 # --- PySpark ---
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *  # col, lit, when, sum, etc. para transformaciones de DataFrames
 
-# --- Cargar variables de entorno desde .env ---
-from dotenv import load_dotenv
-
-# --- Conexión a Astra DB ---
-from astrapy import DataAPIClient  # Librería oficial de DataStax para Astra DB
+# --- Análisis y visualización ---
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 # --- Funciones ETL auxiliares ---
@@ -52,3 +53,43 @@ def insertar_si_vacia(df: pd.DataFrame, coleccion: str, db):
         print(f"✅ Insertados {len(records)} registros en '{coleccion}'.")
     else:
         print(f"ℹ️ La colección '{coleccion}' ya contiene datos. No se hicieron inserciones.")
+
+
+def get_hash_value(input_str):
+    """
+    Calcula el valor hash de una cadena de entrada.
+
+    Parameters:
+        input_str (str): La cadena de entrada para la cual se calculará el hash.
+
+    Returns:
+        str: El valor hash calculado
+    """
+    cleaned_input = input_str.lower().strip()
+    hash_object = hashlib.sha256(cleaned_input.encode())
+    hash_value = hash_object.hexdigest()
+    return hash_value
+
+
+def hash_column(df, column_name):
+    """
+    Aplica el algoritmo "Hash" a los valores en una columna del DataFrame.
+
+    Parameters:
+        df (pd.DataFrame): El DataFrame que contiene los datos.
+        column_name (str): El nombre de la columna que se va a hashear.
+
+    Returns:
+        pd.DataFrame: El DataFrame modificado con los valores hasheados en la columna especificada.
+    """
+    try:
+
+        # Crear una nueva columna _hashed con los valores hasheados de la columna recibida
+        df.loc[:, f"{column_name}_hashed"] = df[column_name].apply(
+            lambda row: get_hash_value(row) 
+            )
+        return df
+
+    except KeyError:
+        print(f"La columna '{column_name}' no existe en el DataFrame.")
+        return df
